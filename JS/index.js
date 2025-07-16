32bcf350363ac024ecb7faacd35d8fca3be8b32b
Angel David Garcez Di Luzio <angeldiluzio@gmail.com>
2025-07-16 18:24:50 -0400

    Agregando el localStorage

--- JS/index.js
+++ JS/index.js
@@ -10,10 +10,11 @@ btn.addEventListener("click", () => {
         const li = document.createElement("li");
         const span = document.createElement("span");
         span.textContent = taskText;
-
+        
         //Marcar como completa
         span.addEventListener("click", () => {
             span.classList.toggle("completada");
+            guardar();
         })
 
         //Boton eliminar
@@ -27,8 +28,10 @@ btn.addEventListener("click", () => {
         btnBorrar.style.background = "transparent";
         btnBorrar.style.cursor = "pointer";
 
-        btnBorrar.addEventListener("click", () => {
+        btnBorrar.addEventListener("click", (e) => {
+            e.stopPropagation();
             list.removeChild(li);
+            guardar();
         });
 
         //Mostrar en el DOM
@@ -38,6 +41,63 @@ btn.addEventListener("click", () => {
 
         //Reseteo del input
         task.value = "";
+
+        guardar();
     }
 });
 
+function guardar(){
+    const tareas = [];
+    document.querySelectorAll("#list li").forEach(li => {
+        tareas.push({
+            tarea: li.firstChild.textContent,
+            completada: li.firstChild.classList.contains("completada")
+        });
+    });
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+    console.log(localStorage.getItem("tareas"));
+}
+
+function cargar(){
+    const tareas = JSON.parse(localStorage.getItem("tareas")) || [];
+    tareas.forEach(taskObj => {
+        const li = document.createElement("li");
+        const span = document.createElement("span");
+        span.textContent = taskObj.tarea;
+        console.log(taskObj);
+        if (taskObj.completada) {
+            span.classList.add("completada");
+        }
+
+                //Marcar como completa
+        span.addEventListener("click", () => {
+            span.classList.toggle("completada");
+            guardar();
+        })
+
+        //Boton eliminar
+        const btnBorrar = document.createElement("button");
+        btnBorrar.textContent = "❌";
+        btnBorrar.style.width = "30px";
+        btnBorrar.style.textAlign = "center";
+        btnBorrar.style.padding = "0.5rem";
+        btnBorrar.style.marginLeft = "10px";
+        btnBorrar.style.border = "none";
+        btnBorrar.style.background = "transparent";
+        btnBorrar.style.cursor = "pointer";
+
+        btnBorrar.addEventListener("click", (e) => {
+            e.stopPropagation();
+            list.removeChild(li);
+            guardar();
+        });
+
+        //Mostrar en el DOM
+        li.appendChild(span);
+        li.appendChild(btnBorrar);
+        list.appendChild(li);
+
+        
+    })
+}
+cargar();
